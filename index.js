317ab617d3da8fde01df6c198c649a92242d00a8
EA - Ana Beatriz Alves De Siqueira <siqueira_beatriz@educadventista.org>
2025-08-14 11:57:09 -0300

    aula henrique

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import express from 'express'
 const app = express()
 const PORT = 3000
 
+let usuarios = [];
+let contador = 1;
+
 app.use(express.json())
 
 //ROTA DE TESTE
@@ -10,6 +13,39 @@ app.get("/status", (req, res) => {
   res.json({message: "API Online"})
 })
 
+//ROTA GET LISTAR TODOS OS ITENS
+app.get("/usuarios") , (req,res) => {
+  res.json(usuarios)
+}
+
+//ROTA  POST CRIAR NOVO USUARIO
+app.post("/usuarios", (req,res) => {
+   const { nome} = req.body;
+
+   const novoUsuario = {
+    id: contador ++,
+    nome
+   };
+
+   usuarios.push(novoUsuario);
+   res.status(201).json(novoUsuario);
+
+})
+//ROTA PUT ATUALIZA USUARIO POR ID
+app.put("/usuarios/:id", (req, res) =>{
+  const id = parseInt(req.params.id)
+  const {nome} = req.body
+
+  const usuaio = usuarios.find((i) => i.id === id );
+
+  if (!usuario){
+    return res.status(404). json({ erro: "Usuario não encontrado"});
+  }
+  usuario.nome = nome;
+  res.json(usuario);
+});
+
+  
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`)
 })
